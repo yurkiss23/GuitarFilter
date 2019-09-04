bff67ed78d43e128dce2074dfc24f504591c7147
yurkiss <terranet23@gmail.com>
2019-09-04 21:50:32 +0300

    Добавьте файлы проекта.

---
+++ GuitarFilter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuitarFilter.MvcApplication" Language="C#" %>
---
+++ GuitarFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GuitarFilter.Startup))]
+namespace GuitarFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
